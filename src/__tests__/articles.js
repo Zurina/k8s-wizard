a897c534347a04209028052fafca17105067d2e1
Mathias Bigler <orchidevej@hotmail.com>
2022-06-28 15:27:13 +0200

    feat: impl test

---
+++ src/__tests__/articles.js
@@ -0,0 +1,22 @@
+import "@testing-library/jest-dom";
+import * as React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import ArticleRow from "../utilities/articleRow"
+import ClickableArticleRow from "../utilities/clickableArticleRow"
+
+test("Article row", () => {
+	render( <ArticleRow name={"Mathias"}/>);
+	expect(screen.queryByText("hello")).toBeNull()
+	expect(screen.queryByText("Mathias")).toBeTruthy()
+});
+
+test("ClickableArticle row", () => {
+	const spy = jest.fn();
+	const { container } = render( <ClickableArticleRow name={"Mathias"} updateEnvState={spy} push={spy}/>);
+	expect(screen.queryByText("hello")).toBeNull()
+	expect(screen.queryByText("Mathias")).toBeTruthy()
+
+	const row = container.querySelector('div')
+	fireEvent.click(row);
+	expect(spy).toHaveBeenCalled();
+});
