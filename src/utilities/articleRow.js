cef40977a86bd86bc3402bb7ff365f01b97d06f2
Mathias Bigler <orchidevej@hotmail.com>
2022-08-29 22:41:39 +0200

    refactor: remove icon from articlerow

--- src/utilities/articleRow.js
+++ src/utilities/articleRow.js
@@ -14,11 +14,7 @@ const ArticleRow = (props) => {
 
 	return (
 		<article className="leaderboard__profile">
-			<img
-				src="https://randomuser.me/api/portraits/men/36.jpg"
-				alt="Mark Zuckerberg"
-				className="leaderboard__picture"
-			/>
+			{/* <h2 className="leaderboard__picture">{props.name.substring(0, 1)}</h2> */}
 			<span className="leaderboard__name">{props.name}</span>
 			<button className="learn-more" onClick={onLearnMoreClick}>Learn more</button>
 		</article>
